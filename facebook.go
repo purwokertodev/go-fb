2f195eb47b7a46bfed513a63fcfc39ad346c8611
Wuriyanto <wuriyanto48@yahoo.co.id>
2017-07-20 16:59:23 +0700

    fixing test

--- facebook.go
+++ facebook.go
@@ -1,11 +1,10 @@
 package fb
 
 import (
-	"net/http"
-	"time"
 	"encoding/json"
 	"errors"
-	"fmt"
+	"net/http"
+	"time"
 )
 
 const (
@@ -37,7 +36,7 @@ func (f *Facebook) GetAppAccessToken() (*AccessTokenApp, error) {
 	if f.beta {
 		betaApp = "beta."
 	}
-	uri := GRAPH_BASE_URL +betaApp+ "facebook.com/" + f.version + "/oauth/access_token?client_id=" + f.appId + "&client_secret=" + f.appSecret + "&grant_type=client_credentials"
+	uri := GRAPH_BASE_URL + betaApp + "facebook.com/" + f.version + "/oauth/access_token?client_id=" + f.appId + "&client_secret=" + f.appSecret + "&grant_type=client_credentials"
 	response, err := client.Get(uri)
 	if err != nil {
 		return nil, err
