4046144cb0feb12f009269be2b35a2d516c889d5
Wuriyanto <wuriyanto48@yahoo.co.id>
2017-07-23 00:26:31 +0700

    fixing test

--- facebook_test.go
+++ facebook_test.go
@@ -1,12 +1,28 @@
 package fb
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"testing"
 )
 
-func testNewFacebook(t *testing.T) {
+func TestNewFacebook(t *testing.T) {
 	f := NewFacebook("001", "secret", "v1.1", "http://localhost:3000/auth/facebook/callback", false, 1000)
 	if f == nil {
 		t.Error("failed create new Facebook")
+	}
+}
+
+func TestGetSecretProof(t *testing.T) {
+	f := NewFacebook("001", "secret", "v1.1", "http://localhost:3000/auth/facebook/callback", false, 1000)
+
+	key := []byte("secret")
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte("666666"))
+	expectedHex := hex.EncodeToString(h.Sum(nil))
+
+	if expectedHex != f.GetSecretProof("666666") {
+		t.Error("failed to verify secretProof")
 	}
 }
